23b05a5555e39d3514c4b436fd99daae8aeca6fc
d1_12345_nilesh <nilesh@sunbeaminfo.com>
2024-03-06 15:25:04 +0530

    taken user input

--- Addition.java
+++ Addition.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Addition {
     public static void main(String[] args) {
-        int a = 22, b = 7, c;
+        int a, b, c;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+        a = sc.nextInt();
+        b = sc.nextInt();
         c = a + b;
         System.out.println("Addition: " + c);
     }
